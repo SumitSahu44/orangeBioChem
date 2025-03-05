a15caab8e2662d77a592719516a26c2b16a9a5f5
SumitSahu444 <sumitkumarsahu141@gmail.com>
2025-03-05 23:43:53 +0530

    service fix

--- src/component/Services.jsx
+++ src/component/Services.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Services = () => {
   return (
     <div>
-      <div class="container w-[90%] mx-auto  px-4 py-8">
+      <div class="container w-[90%] mx-auto my-15 px-4 py-8">
    <h1 class="text-3xl font-bold mb-2">
     We Offer a Wide Range of Services
    </h1>
@@ -11,7 +11,7 @@ const Services = () => {
     With constant innovation and understanding of specific market needs,
    </p>
    <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-    <div class="bg-green-100 p-6 rounded-lg text-center">
+    <div class="bg-[#5ACE8E] p-6 rounded-lg text-center">
      <img alt="Icon representing range of products" class="mx-auto mb-4" height="50" src="https://storage.googleapis.com/a1aa/image/Z0bLknhlAg2uPQrawxjI7i_1j_hA75ngHWTPIIu3zmg.jpg" width="50"/>
      <h2 class="text-xl font-semibold mb-2">
       Range of Products
@@ -20,8 +20,8 @@ const Services = () => {
       With constant innovation and understanding of specific market needs, we have evolved a wide range of products to meet our customers' diverse needs globally.
      </p>
     </div>
-    <div class="bg-yellow-100 p-6 rounded-lg text-center">
-     <img alt="Icon representing quality" class="mx-auto mb-4" height="50" src="https://storage.googleapis.com/a1aa/image/BK6eM6RV-63LmKMmEWDADH_zbqMxDLjCVj0gk_Km08s.jpg" width="50"/>
+    <div class="bg-[#EBAC60]  p-6 rounded-lg text-center">
+     <img alt="Icon representing  quality" class="mx-auto mb-4" height="50" src="https://storage.googleapis.com/a1aa/image/BK6eM6RV-63LmKMmEWDADH_zbqMxDLjCVj0gk_Km08s.jpg" width="50"/>
      <h2 class="text-xl font-semibold mb-2">
       Quality
      </h2>
@@ -29,7 +29,7 @@ const Services = () => {
       Since 1992, the Orange Biochem's name has been synonymous with the trust and reliability inherent in the word Quality. Quality is ingrained in the work of our staff and all our Values.
      </p>
     </div>
-    <div class="bg-green-200 p-6 rounded-lg text-center">
+    <div class="bg-[#5ACE8E] p-6 rounded-lg text-center">
      <img alt="Icon representing innovation" class="mx-auto mb-4" height="50" src="https://storage.googleapis.com/a1aa/image/FiAM1jd9g9dDY--Ro60H6QMphzXnqZCqthEWIV3Vd6o.jpg" width="50"/>
      <h2 class="text-xl font-semibold mb-2">
       Innovation
